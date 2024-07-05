e37693ebbd97232207193a4aceb9707f10213869
bluVshal <blu.vishal@gmail.com>
2024-07-05 19:43:49 +0400

    Added templates for Contact and About and modified search button css

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,5 +4,13 @@ const router = express.Router();
 router.get('/',(req, res) => {
     res.render("index");
 });
+router.get('/about',(req, res) => {
+    res.render("./layouts/about");
+});
+
+router.get('/contact',(req, res) => {
+    res.render("./layouts/contactus");
+});
+
 
 module.exports = router;
